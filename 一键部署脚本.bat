3a08b0103009d622c486bb5b8df0a40aea52cbd4
Alpherg <99066610+avilliai@users.noreply.github.com>
2024-09-17 13:53:31 +0800

    更新 一键部署脚本.bat

--- 一键部署脚本.bat
+++ 一键部署脚本.bat
@@ -7,6 +7,5 @@ where python
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 pause
 cd ../..
-venv/Scripts/python.exe -m pip install pip==24.0
 pip install -r requirements_win.txt
 pause
